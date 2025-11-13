4865609daf273a481f7eb597c80f5ef16cfb0efa
Topi Ahola <topi.ahola@metropolia.fi>
2025-11-13 12:02:13 +0200

    Greetings.jsx esimerki
    -Topi yoga7

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Footer from "./components/Footer.jsx";
 //funktio joka palauttaa JSX (javascriptxml)
 const App = () => {
 
-
+    const name = 'Topi';
+    const age = 69;
+    const isTeacher = true;
     return (
         <>
             <h1>My App</h1>
-            <Greeting/>
+            <Greeting name={name} age={age} isTeacher={isTeacher} />
             <ul>
                 <li>1. jutu</li>
                 <li>2. jutu</li>
--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,5 +1,16 @@
-const Greeting = ()=>{
-    return <div>Greetings</div>;
+const Greeting = (props)=>{
+
+    console.log(props);
+    const { name, age, isTeacher } = props;
+    const teacherFunc = (isTeacher) =>{ if(isTeacher){return 'you are teacher';} else {return 'you are not a teacher';} }
+    let teacherString = teacherFunc(isTeacher);
+
+    //funktiokutsu nimellä toimii mutta nulofunktio ei...
+    return (
+        <div>
+            Greetings {name}, {age}.
+            {teacherFunc(isTeacher)}
+        </div>);
 }
 
 export default Greeting;
