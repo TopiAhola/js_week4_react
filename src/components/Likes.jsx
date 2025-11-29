2afe9bdf6e955dc0c3a8d2e0e36901aba69ae846
TopiAhola <topi.ahola@metropolia.fi>
2025-11-29 21:59:14 +0200

    Likes.jsx käyttäjän likearray kesken
    -Topi nuc

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -12,7 +12,7 @@ const Likes = ({ item }) => {
   //state
   const [likes, setLikes] = useState(0);
   const [userLikes, setUserLikes] = useState(false);
-
+  const [userLikesId, setUserLikesId] = useState(0);
 
 
   useEffect(( ) => {
@@ -31,10 +31,21 @@ const Likes = ({ item }) => {
     const fetchUserLikes = async (user)=> {
       try {
         if(user !== null){
-          const userLikesId = await getLikesByUser(user.user_id);
-          console.log('fetchUserLikes like value:',userLikesId);
-          setUserLikes(userLikesId)
+          const userLikesArray = await getLikesByUser(user.user_id);
+          if(userLikesArray.length > 0){
+            setUserLikesId(userLikesArray[0].media_id);
+            console.log('fetchUserLikes like media_id:',userLikesId);
+
+            //set like status for this item
+            (userLikesId===item.media_id) ? setUserLikes(true) : setUserLikes(false);
+
+          } else {
+            console.log('fetchUserLikes like array length 0');
+          }
+        } else {
+          console.log('fetchUserLikes for null user!');
         }
+
       } catch (error) {
         console.log(error);
         setLikes(0);
@@ -46,9 +57,23 @@ const Likes = ({ item }) => {
   }, []);
 
 
+  const toggleLike = async ()=> {
+    console.log('toggle like target: '+item.media_id+' user likes: '+userLikes );
+    if (userLikes){
+      //dislike
+
+    } else {
+      //like this item
+
+    }
+
+
+  }
+
+
   return (
     <div>
-      <button onClick={() => {}} className={userLikes ? "bg-red-500" : "bg-green-500"}>
+      <button onClick={toggleLike} className={userLikes ? "bg-red-500" : "bg-green-500"}>
         <div>Likes: {likes}</div>
         <div>{userLikes ? "Dislike" : "Like"}</div>
       </button>
