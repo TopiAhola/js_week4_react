85118810f73dc302737b38dfb73e0f55ef41d198
TopiAhola <topi.ahola@metropolia.fi>
2025-12-04 18:17:54 +0200

    tailwind palautus
    -Topi nuc

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 
 const Layout = () => {
 
-  const  {handleLogin, handleAutoLogin } = useUserContext();
+  const  {handleAutoLogin } = useUserContext();
   const { user } = useUserContext();
 
   useEffect(
